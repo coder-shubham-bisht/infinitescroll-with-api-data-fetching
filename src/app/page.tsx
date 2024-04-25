283d3f782b3a816efbbb625775c83a0d5146a089
Shubham Bisht <bistshubham2016@gmail.com>
2024-04-25 22:43:44 +0530

    seo improved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,13 @@ export default async function Home() {
   const initialAnimes = await getAllAnime(1, INITIAL_NUMBER_OF_USERS);
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 max-w-[1000px] mx-auto gap-1 md:gap-3">
-      <UserList initialAnimes={initialAnimes} />
-    </div>
+    <>
+      <h1 className="text-center font-bold text-2xl mb-4">
+        Infinite Scoll with Server Action
+      </h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 max-w-[1000px] mx-auto gap-1 md:gap-3">
+        <UserList initialAnimes={initialAnimes} />
+      </div>
+    </>
   );
 }
